8d946ce48c46e44f7413ab01b6d393a904fbcfc3
Hojung Youn <amoc.yn@gmail.com>
2012-11-29 02:49:43 +0900

    command: add lexer routine

--- lib/Amolog/Command.pm
+++ lib/Amolog/Command.pm
@@ -1,9 +1,47 @@
 package Amolog::Command;
+use Data::Dump qw(dump);
 
 sub run {
     my @args = @_;
     my @tokens = lex @args;
     return parse @tokens;
+}
+
+sub lex {
+    my @args = @_;
+    my @tokens;
+    while ($_ = shift @args) {
+        if (/^-(day|date|month|year)$/) {
+            my $range = shift @args;
+            push @tokens, "CondDate";
+        }
+        elsif (/^-text$/) {
+            my $text = shift @args;
+            push @tokens, "CondText";
+        }
+        elsif (/^-(user|who)$/) {
+            push @tokens, "CondUser";
+        }
+        elsif (/^-(!|n|not)$/) {
+            push @tokens, "Not";
+        }
+        elsif (/^-(a|and)$/) {
+            push @tokens, "And";
+        }
+        elsif (/^-(o|or)$/) {
+            push @tokens, "Or";
+        }
+        elsif ($_ eq "(") {
+            push @tokens, "ParenOpen";
+        }
+        elsif ($_ eq ")") {
+            push @tokens, "ParenClose";
+        }
+        else {
+            die "Unexpected option: $_";
+        }
+    }
+    @tokens;
 }
 
 1;
